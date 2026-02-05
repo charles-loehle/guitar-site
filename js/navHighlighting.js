93b564b8dbca7a7a719289e13a4ce06aa662a15c
Charles Loehle <clguitar1@gmail.com>
2026-02-05 11:33:54 -0500

    added contact form

--- js/navHighlighting.js
+++ js/navHighlighting.js
@@ -2,7 +2,6 @@
 
 const sections = document.querySelectorAll('section');
 const navLi = document.querySelectorAll('nav ul li');
-// console.log(navLi);
 
 window.addEventListener('scroll', () => {
 	console.log('pageYOffset: ' + window.pageYOffset);
@@ -11,17 +10,14 @@ window.addEventListener('scroll', () => {
 	sections.forEach(section => {
 		// get distance from top of page to the current section
 		const sectionTop = section.offsetTop;
-		console.log('sectionTop: ' + sectionTop);
 
 		// get the height of each section
 		const sectionHeight = section.clientHeight;
-		console.log('section height: ' + sectionHeight);
 
 		// if the distance scrolled is past a specified value, get the current sections's id
 		// pageYOffset = returns the number of pixels the document is currently scrolled along the vertical axis (that is, up or down)
 		if (pageYOffset >= sectionTop - sectionHeight / 25) {
 			current = section.getAttribute('id');
-			// console.log(sectionTop - sectionHeight / 12);
 		}
 	});
 
